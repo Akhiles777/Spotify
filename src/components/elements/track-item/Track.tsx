e623bfe045d6b09dd81f6d2fe6eb5452980d6cdc
Gasan <gasanalunkachev@gmail.com>
2025-11-16 19:14:23 +0300

    ff

--- src/components/elements/track-item/Track.tsx
+++ src/components/elements/track-item/Track.tsx
@@ -1,50 +1,56 @@
+import { observer } from 'mobx-react-lite'
 import { Ellipsis, Heart } from 'lucide-react'
-import type  {ITrack} from '../../../types/track.types'
+import type { ITrack } from '../../../types/track.types'
 
 import TrackInfo from '../../ui/track-info/TrackInfo'
-
 
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
 import { transformDuration } from '@/utils/transform-duration'
 import { favoriteStore } from '@/store/favorite-store'
 
-
 // activate utc plugin so dayjs.unix(...).utc() works and utc is actually used
 dayjs.extend(utc)
 
-interface Props{
-track: ITrack
+interface Props {
+  track: ITrack
 }
 
-
-export  default function  Track ({track} :Props)  {
-
-    return ( 
-
+const Track = observer(({ track }: Props) => {
+  return (
     <div className='border-b border-white/10 py-7 flex justify-between items-center last:border-0'>
-      
 
       {/*Track info*/}
+      <TrackInfo
+        track={track}
+        title={track.name}
+        subTitle={transformDuration(track.duration)}
+      />
 
-<TrackInfo track={track} title={track.name} subTitle={transformDuration(track.duration)}/>
+      <div>
+        <button
+          onClick={() => {
+            favoriteStore.toggleFavorite(track.name)
+          }}
+          className='mr-8'
+        >
+          <Heart
+            className='text-(--color-primary) hover:opacity-100 opacity-85 hover:fill-(--color-primary) duration-300'
+            fill={
+              favoriteStore.favoritesName.includes(track.name)
+                ? 'var(--color-primary)'
+                : ''
+            }
+          />
+        </button>
 
-    <div>
-
-  <button onClick={() => {
-    favoriteStore.toggleFavorite(track.name)
-  }} className='mr-8'>
-    <Heart className='text-(--color-primary) hover:opacity-100 opacity-85 hover:fill-(--color-primary) duration-1000 ' fill={favoriteStore.favoritesName.includes(track.name)? 'var(--color-primary)' : 'none'}/>
-  </button>
-
-  <button>          
-        <Ellipsis className='mr-4 opacity-30 duration-300 hover:opacity-100' />
-  </button>
+        <button>
+          <Ellipsis className='mr-4 opacity-30 duration-300 hover:opacity-100' />
+        </button>
+      </div>
 
     </div>
+  )
+})
 
-
-    </div>
-    )
-
-}
+export default Track
